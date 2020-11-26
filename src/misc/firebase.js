95f9eea22d7098e06741d0d82e9b29f2de5d604a
lunatic <monudaksh09@gmail.com>
2020-11-26 21:34:16 +0530

    initialized firebase projects

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyAZ_xCNW0pYQnD-wZTeGLka02Fsxc5jCKw',
+  authDomain: 'chat-web-app-3ef88.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-3ef88.firebaseio.com',
+  projectId: 'chat-web-app-3ef88',
+  storageBucket: 'chat-web-app-3ef88.appspot.com',
+  messagingSenderId: '765760476311',
+  appId: '1:765760476311:web:ac121f1c613839d62085d2',
+};
+
+const app = firebase.initializeApp(config);
