260eb22c49c59a21a7a268edaaac4182a3f7e6db
lunatic <monudaksh09@gmail.com>
2020-11-30 08:39:18 +0530

    started to wrok with chat room list

---
+++ src/components/rooms/RoomItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import TimeAgo from 'timeago-react';
+
+const RoomItem = () => {
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-item-center">
+        <h3 className="text-disappear"> room name </h3>
+        <TimeAgo datetime={new Date()} className="font-normal text-black-45" />
+      </div>
+
+      <div className="d-flex align-items-center text-black-70">
+        <spna>No message yet...</spna>
+      </div>
+    </div>
+  );
+};
+
+export default RoomItem;
