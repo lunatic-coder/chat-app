07a0c46181f42732dfae16cf6b6b7a76b03bdcf2
lunatic <monudaksh09@gmail.com>
2020-12-02 08:19:37 +0530

    fixed problem in removed and granted premission logic

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router';
+import { Alert } from 'rsuite';
 
 import { database } from '../../../misc/firebase';
 import { transformToArrWithId } from '../../../misc/helpers';
@@ -29,12 +30,38 @@ const Message = () => {
     };
   }, [chatId]);
 
+  const handleAdmin = useCallback(
+    async uid => {
+      const adminsRef = database.Ref(`/rooms/${chatId}/admins`);
+
+      let alertMsg;
+
+      await adminsRef.transaction(admins => {
+        if (admins) {
+          if (admins[uid]) {
+            admins[uid] = null;
+            alertMsg = 'Admin premission removed';
+          } else {
+            admins[uid] = true;
+            alertMsg = 'Admin premission granted';
+          }
+        }
+        return admins;
+      });
+
+      Alert.info(alertMsg, 4000);
+    },
+    [chatId]
+  );
+
   return (
     <ul className="msg-list custom-scroll">
       {isChatEmpty && <li>No Message yet</li>}
 
       {canShowMessage &&
-        messages.map(msg => <MessageItem key={msg.id} messages={msg} />)}
+        messages.map(msg => (
+          <MessageItem key={msg.id} messages={msg} handleAdmin={handleAdmin} />
+        ))}
     </ul>
   );
 };
